0266cd488c32e0e6fa6c129852900b7f7a1da1a5
Mark-Durkot <106492009+Studio-Jam@users.noreply.github.com>
2023-04-27 04:25:28 +0200

    Fix container detach

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@
 docker stop transliterator-backend
 docker rm transliterator-backend
 docker rmi markdurkot/transliterator-backend:latest
-docker run -p 8080:8080 -p 1234:1234 --name=transliterator-backend -it markdurkot/transliterator-backend:latest
+docker run -d -p 8080:8080 -p 1234:1234 --name=transliterator-backend -it markdurkot/transliterator-backend:latest

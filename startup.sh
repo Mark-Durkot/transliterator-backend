aef571d263f7f586acac5f37d6e5456b59f50c78
Mark-Durkot <106492009+Studio-Jam@users.noreply.github.com>
2023-04-27 04:19:26 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,4 +4,3 @@ docker stop transliterator-backend
 docker rm transliterator-backend
 docker rmi markdurkot/transliterator-backend:latest
 docker run -p 8080:8080 -p 1234:1234 --name=transliterator-backend -it markdurkot/transliterator-backend:latest
-docker logs transliterator-backend > docker.log 2>&1
